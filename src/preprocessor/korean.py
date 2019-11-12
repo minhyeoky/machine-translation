d044d080b425f5a1c36fab6dfba2cf8bb393cbc3
minhyeoklee <rhodochrosited@gmail.com>
2019-11-13 02:17:14 +0900

    preprocessor

---
+++ src/preprocessor/korean.py
@@ -0,0 +1,33 @@
+from konlpy.tag import Mecab
+from collections import namedtuple
+import re
+from .base import PreprocessorBase
+
+
+class KorPreprocessor(PreprocessorBase):
+    def __init__(self):
+        super(KorPreprocessor, self).__init__()
+
+        self.tagger = Mecab()
+
+    def _to_morphs(self, s):
+        return self.tagger.pos(s)
+
+    @staticmethod
+    def _clean(s):
+        s = re.sub(r'[^가-힣ㄱ-ㅎ?.!,]', ' ', s)
+        s = s.strip()
+        return s
+
+    def preprocess(self, s):
+        s = self._basic_nmt(s)
+        s = self._clean(s)
+
+        tagged = self._to_morphs(s)
+
+        _s = []
+        for w, _ in tagged:
+            _s.append(w)
+        s = ' '.join(_s)
+        s = self._add_token(s)
+        return s
---
+++ tests/test_preprocessor.py
@@ -0,0 +1,26 @@
+import unittest
+from typing import NamedTuple
+
+from src.preprocessor.english import EngPreprocessor
+from src.preprocessor.korean import KorPreprocessor
+
+
+class TestPreprocessor(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.preprocessor = NamedTuple('Preprocessor', [('kor', KorPreprocessor), ('eng', EngPreprocessor)])
+        self.preprocessor.kor = KorPreprocessor()
+        self.preprocessor.eng = EngPreprocessor()
+
+    def test_eng_preprocessor(self):
+        self.assertEqual(self.preprocessor.eng.preprocess('he is a boy.'), '<start> he is a boy . <end>')
+        self.assertEqual(self.preprocessor.eng.preprocess('May I borrow this book?'),
+                         '<start> may i borrow this book ? <end>')
+
+    def test_kor_preprocessor(self):
+        self.assertEqual(self.preprocessor.kor.preprocess('안녕하세요?'), '<start> 안녕 하 세요 ? <end>')
+        self.assertEqual(self.preprocessor.kor.preprocess('제가 이 책을 빌려도 되나요?'), '<start> 제 가 이 책 을 빌려 도 되 나요 ? <end>')
+
+
+if __name__ == "__main__":
+    unittest.main()
