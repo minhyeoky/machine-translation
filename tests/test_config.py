f9c16165d4cbb617a16e44da995c090f585ba040
minhyeoklee <rhodochrosited@gmail.com>
2019-11-14 22:38:15 +0900

    add test_config

---
+++ tests/test_config.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+from src.config import Config
+
+os.chdir('..')
+
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.json_file = 'config/test_config.json'
+
+    def test_from_to_json_file(self):
+        to_json_file = 'tests/test_to_json.json'
+        config = Config.from_json_file(self.json_file)
+        self.assertIsInstance(config, Config)
+        self.assertEqual(config.name, 'test')
+        config.to_json_file(to_json_file)
+        config2 = Config.from_json_file(to_json_file)
+
+        self.assertEqual(config.name, config2.name)
+
+
+if __name__ == "__main__":
+    unittest.main()
